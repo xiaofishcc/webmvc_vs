624af957b10006b261a01128326eb76e4e973451
xiaofishcc <xiaofishcc1123>
2017-05-11 19:01:16 +0800

    多线程

---
+++ demoduoxianc/demoduoxianc/demoduoxianc.h
@@ -0,0 +1,32 @@
+
+// demoduoxianc.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CdemoduoxiancApp: 
+// 有关此类的实现，请参阅 demoduoxianc.cpp
+//
+
+class CdemoduoxiancApp : public CWinApp
+{
+public:
+	CdemoduoxiancApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CdemoduoxiancApp theApp;
