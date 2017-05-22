cd421584aef94b091bfacf570254681757f5f9f8
xiaofishcc <xiaofishcc1123>
2017-05-22 16:48:32 +0800

    开机图片显示

---
+++ SplashDemo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace SplashDemo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            //Thread to show splash window
+            Thread thUI = new Thread(new ThreadStart(ShowSplashWindow));
+            thUI.Name = "Splash UI";
+            thUI.Priority = ThreadPriority.Normal;
+            thUI.IsBackground = true;
+            thUI.Start();                      
+            
+            //Thread to load time-consuming resources.
+            Thread th = new Thread(new ThreadStart(LoadResources));
+            th.Name = "Resource Loader";
+            th.Priority = ThreadPriority.Highest;
+            th.Start();
+
+            th.Join();
+
+            if (SplashForm!=null)
+            {
+                SplashForm.Invoke(new MethodInvoker(delegate { SplashForm.Close(); }));
+            }
+
+            thUI.Join();
+            Application.Run(new frm_Main());         
+        }
+      
+        public static frm_Splash SplashForm
+        {
+            get;
+            set;
+        }
+
+        private static void LoadResources()
+        {            
+            for (int i = 1; i <=15; i++)
+            {
+                if (SplashForm != null)
+                {
+                    SplashForm.Invoke(new MethodInvoker(delegate { SplashForm.lblStatus.Text = "Loading some things... " + DateTime.Now.ToString(); }));
+                }
+                Thread.Sleep(30000);               
+            }
+            SplashForm.Invoke(new MethodInvoker(delegate { SplashForm.lblStatus.Text = "Done. " + DateTime.Now.ToString(); }));
+        }
+
+        private static void ShowSplashWindow()
+        {
+            SplashForm = new frm_Splash();
+            Application.Run(SplashForm);
+        }
+    }
+}
---
+++ SplashDemo/frm_Splash.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace SplashDemo
+{
+    partial class frm_Splash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Splash));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rock\' Splash Demo";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.Color.Transparent;
+            this.lblStatus.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblStatus.Location = new System.Drawing.Point(16, 234);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(203, 12);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Status: Connecting to database...";
+            // 
+            // frm_Splash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(458, 264);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_Splash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frm_Splash";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label lblStatus;
+
+    }
+}
