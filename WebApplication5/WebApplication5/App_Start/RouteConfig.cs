7203f9c1681fed2be0e59a7c19a30bd7cd74a45b
xiaofishcc <xiaofishcc1123>
2017-06-02 11:44:44 +0800

    web

---
+++ WebApplication5/WebApplication5/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication5
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)//静态方法，这就是映射路由的控制，这个方法定义了路由规则
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",//url: "{controller}/{action}/{id}"定义了URL的格式
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
